8759a88e7869f5ff6d0eca499e0698a6da3cfc84
Akinboboye Akinwande <Akinboboye.Akinwande@fbnquest.com>
2023-12-09 01:17:32 +0100

    began working on layer-6

--- src/app/pages/homepage/homepage/layer-6/layer-6.component.ts
+++ src/app/pages/homepage/homepage/layer-6/layer-6.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-layer-6',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './layer-6.component.html',
-  styleUrl: './layer-6.component.css'
+  styleUrl: './layer-6.component.css',
 })
 export class Layer6Component {
-
+  faFacebook=faFacebook
+  faTwitter=faTwitter
+  faInstagram=faInstagram
+  faYoutube=faYoutube
+  faLinkedin=faLinkedin
 }
